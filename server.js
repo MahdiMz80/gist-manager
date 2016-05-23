67b453d49f4af88ae6002163f6ef45153083d591
Joe Karlsson <joe.carlson@callmejoe.net>
2016-05-23 11:11:19 -1000

    update.

--- server.js
+++ server.js
@@ -115,7 +115,7 @@
   } else {
     app.use(express.static(__dirname + '/dist'));
     app.get('*', function response(req, res) {
-      res.sendFile(path.join(__dirname, 'dist/index.html'));
+      res.sendFile('./dist/index.html');
     });
   }
 
